9d3f6a096da4d4b856ab1788cc1c0c4aee966aae
daniel <fundan.fundan@gmail.com>
2023-10-31 22:29:15 +0300

    Change inCreateView using binding

--- app/src/main/java/com/example/tolkienmaps/MiddleEarthMap.kt
+++ app/src/main/java/com/example/tolkienmaps/MiddleEarthMap.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.tolkienmaps.databinding.FragmentMiddleEarthMapBinding
 
 class MiddleEarthMap : Fragment() {
+    private lateinit var binding: FragmentMiddleEarthMapBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         // TODO pick a style and use one
-        return inflater.inflate(R.layout.fragment_middle_earth_map, container, false)
+        binding = FragmentMiddleEarthMapBinding.inflate(layoutInflater)
+        return binding.root
     }
 }
