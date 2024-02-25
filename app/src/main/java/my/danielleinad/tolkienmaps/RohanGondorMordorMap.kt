e5e37d359ddff4e7968c6663c6b9aeaee415ef9b
daniel <fundan.fundan@gmail.com>
2024-02-25 16:13:21 +0300

    Add Rohan, Gondor, Mordor map

--- app/src/main/java/my/danielleinad/tolkienmaps/RohanGondorMordorMap.kt
+++ app/src/main/java/my/danielleinad/tolkienmaps/RohanGondorMordorMap.kt
@@ -1,19 +1,3 @@
 package my.danielleinad.tolkienmaps
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
-
-class RohanGondorMordorMap : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        // TODO pick a style and use one
-        return inflater.inflate(R.layout.fragment_rohan_gondor_mordor_map, container, false)
-    }
-}
+class RohanGondorMordorMap : TolkienMapFragment("rohan_gondor_mordor")
